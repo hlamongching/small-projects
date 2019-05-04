06acbe97abfd4457a7bc27e56114f2fa1403af7c
hlamongching <hlamongrox@gmail.com>
2019-05-04 22:46:07 +0600

    Add files via upload

---
+++ practise1.php
@@ -0,0 +1,18 @@
+<?php
+$age=18;
+if($age>0&&$age<=18)
+{
+	echo "\nyou are child";
+}
+else if($age>18&&$age<=25)
+{
+	echo "\nyou r young";
+}else if($age>25 &&age<=40)
+{
+	echo "\nyou r matured";
+}
+else{
+	echo "\nsorry ur nt capable for work";
+}
+
+?>
